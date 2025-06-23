b87112bce1f6511075817aedae454b1f13b6bf09
Hwanbit <hwanbit@gmail.com>
2025-06-23 15:10:18 +0900

    Activity: Main, Second, Third

--- app/src/main/java/kr/ac/kopo/activitytest1/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/activitytest1/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +14,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    RadioGroup rg;
+    RadioButton rb_second, rb_third;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +29,19 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        Button btnMain = findViewById(R.id.btn_main);
+        rg = findViewById(R.id.rg);
+        rb_second = findViewById(R.id.rb_second);
+        rb_second.setChecked(true);
+
+        Button btnMain = findViewById(R.id.btn_second);
         btnMain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                Intent intent = null;
+                if(rg.getCheckedRadioButtonId() == R.id.rb_second)
+                    intent = new Intent(getApplicationContext(), SecondActivity.class);
+                else
+                    intent = new Intent(getApplicationContext(), ThirdActivity.class);
                 startActivity(intent);
             }
         });
